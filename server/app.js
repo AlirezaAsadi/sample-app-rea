b580a767755180e24b267c75054fa345611b6a1e
Alireza Asadi <Alireza.Asadi@healthand.com>
2016-06-26 22:43:02 +1000

    Change template engine to ejs to prevent {{ confliction with angular.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,19 @@
 var express = require('express');
-var mustacheExpress = require('mustache-express');
 var app = express();
 app.use(express.static(__dirname + '/../static'));      // Static files
-app.engine('html', mustacheExpress());                  // Template engine extension
-app.set('view engine', 'html');                         // Template engine type         
+app.set('view engine', 'ejs');                          // set the view engine to ejs
 app.set('views', __dirname + '/views');                 // Template path
-
-// Root path handler
-app.use('/', function(req, res){
-    res.render('index')
-});
 
 
 // Get data handler
 app.use('/service/getData', function(req, res){
     var data =  require('./data.json');
     res.send(data);
+});
+
+// Root path handler
+app.use('/', function(req, res){
+    res.render('index')
 });
 
 
