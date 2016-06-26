4224660aa6caeafe3dffec485827a1d658d41c16
Alireza Asadi <Alireza.Asadi@healthand.com>
2016-06-27 00:17:10 +1000

    Add property controller code

--- static/js/modules/index/init.js
+++ static/js/modules/index/init.js
@@ -1,9 +1,63 @@
-define(['jquery','angular','app'], function($,angular,app){
+define(['jquery', 'angular','app'], function($, angular, app){
 
-	// Angular controller goes here
-	// app.controller('contTest', function($scope){
-	//});
+
+	/***********************************\
+		 Mouse over method handler
+	\***********************************/
+	$(document).on('mouseover', '.property-wrapper', function(){
+			$('.property-wrapper').find('.action').removeClass('active');
+			$(this).find('.action').addClass('active');
+	});
+	
+	/***********************************\
+		 Property controller method
+	\***********************************/
+	app.controller('propertyCtl', ['$scope','$http', function($scope, $http) 
+	{
+		$http.post('/service/getData', null,{headers: {'Content-Type': 'application/json'} }).success(
+			function(response) {
+		    	$scope.allProperties = response.results;
+		    	$scope.savedProperties = response.saved;
+			}
+		);
+		
+		/************************\
+		  Add property method
+		\************************/
+		$scope.addProperty = function(property){
+			var alreadyExist = false;
+			// Check if property already added to the savedProperties
+			$scope.savedProperties.find(function(el ,i){
+				if (el.id == property.id){
+					alreadyExist = true;
+				}
+			});
+			
+			if(!alreadyExist)
+				$scope.savedProperties.push(property);
+		};
+		
+		
+		/************************\
+		  Remove property method
+		\************************/
+		$scope.removeProperty = function(property){
+			// Remove property if exists in the savedProperty list
+			$scope.savedProperties.find(function(el ,i){
+				if (el.id == property.id){
+					$scope.savedProperties.splice(i, 1);
+				}
+			});
+		};
+		
+	}]);
 	
 
-	angular.element(document).ready(function() { angular.bootstrap(document, ['rea-sample-app']); });
+
+	/***********************************\
+		 Angular Bootstrap method
+	\***********************************/
+	angular.element(document).ready(function() { 
+		angular.bootstrap(document, ['rea-sample-app']);
+	});
 });
